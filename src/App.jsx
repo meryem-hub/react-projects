bc201a85d8fd494ab34b9e3382b3bb7c6909b836
Meryem Ebrahim <meryemebrahim.46@gmail.com>
2026-01-07 15:56:03 -0800

    add star rating component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Accordion from './components/Accordion'
 import RandomColor from './components/random-color'
+import StarRating from './components/star-rating'
 const App = () => {
   return (
     <div>
-      <Accordion/>
-      <RandomColor/>
+      {/* <Accordion/>
+      <RandomColor/> */}
+      <StarRating/>
     </div>
   )
 }
