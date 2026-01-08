b9da02d16be3d9d87928dd7f426b207d65cef161
Meryem Ebrahim <meryemebrahim.46@gmail.com>
2026-01-08 14:29:38 -0800

    add Image Slider component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Accordion from './components/Accordion'
+import ImageSlider from './components/image-slider'
 import RandomColor from './components/random-color'
 import StarRating from './components/star-rating'
 const App = () => {
   return (
     <div>
-      {/* <Accordion/>
-      <RandomColor/> */}
-      <StarRating/>
+    {/* <Accordion/>
+      <RandomColor/> 
+   <StarRating/>  */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} limit={10}
+      page={"1"}/>
     </div>
   )
 }
